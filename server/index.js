5b67e938cbf89f9225cbed073af1f23b5d954448
torresgol10 <torresgol10.it@gmail.com>
2022-08-22 01:33:52 +0200

    primeros commit.

---
+++ server/index.js
@@ -0,0 +1,38 @@
+//index.js
+import express from 'express';
+//New imports
+import http from 'http'
+import cors from 'cors';
+
+const app = express();
+const PORT = 4000;
+
+http.Server(app);
+
+import socketIO from 'socket.io';
+
+socketIO(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
