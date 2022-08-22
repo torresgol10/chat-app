a2ab0ad3e644d5fcae0c93bda657d9efcbc55524
torresgol10 <torresgol10.it@gmail.com>
2022-08-22 02:06:05 +0200

    Añadimos toda la logica.

--- server/index.js
+++ server/index.js
@@ -1,30 +1,47 @@
-//index.js
-import express from 'express';
-//New imports
-import http from 'http'
-import cors from 'cors';
-
+const express = require('express');
 const app = express();
 const PORT = 4000;
 
-http.Server(app);
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
 
-import { Server } from "socket.io";
-
-const io = new Server(http, {
+const socketIO = require('socket.io')(http, {
     cors: {
         origin: "http://localhost:3000"
     }
-
 });
+let users = [];
 
-//Add this before the app.get() block
-io.on('connection', (socket) => {
+socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('message', (data) => {
+        socketIO.emit('messageResponse', data);
+    });
+
+    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+    //Listens when a new user joins the server
+    socket.on('newUser', (data) => {
+        //Adds the new user to the list of users
+        users.push(data);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit('newUserResponse', users);
+    });
+
     socket.on('disconnect', () => {
         console.log('🔥: A user disconnected');
+        //Updates the list of users when a user disconnects from the server
+        users = users.filter((user) => user.socketID !== socket.id);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit('newUserResponse', users);
+        socket.disconnect();
     });
 });
+
+
 
 app.use(cors());
 
@@ -34,6 +51,6 @@ app.get('/api', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
